6cf37fabac59adb2e692970e2d577d63df2a32be
shigemk2 <i.am.shige@gmail.com>
2016-11-06 16:59:35 +0900

    Add codes

--- src/main/scala/com/example/EnvelopeWrapper.scala
+++ src/main/scala/com/example/EnvelopeWrapper.scala
@@ -1,6 +1,30 @@
 package com.example
 
 import akka.actor._
+import scala.collection.immutable.Map
+
+trait ReplyToSupport {
+  def reply(message: Any) = { }
+  def setUpReplyToSupport(returnAddress: String) = { }
+}
+
+trait RegisterCustomer {
+
+}
+
+trait RabbitMQReplyToSupport extends ReplyToSupport {
+  override def reply(message: Any): Unit = {
+  }
+
+  override def setUpReplyToSupport(returnAddress: String): Unit = {
+  }
+}
+
+case class RegisterCustomerRabbitMQReplyToMapEnvelope(mapMessage: Map[String, String])
+  extends RegisterCustomer with RabbitMQReplyToSupport {
+
+  this.setUpReplyToSupport(mapMessage("returnAddress"))
+}
 
 object EnvelopeWrapperDriver {
 }
